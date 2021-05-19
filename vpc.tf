41f55b7b4ba8118cf22b1cfe4350958f13c8b375
Swat Rajput <swatantrapriya@gmail.com>
2021-05-19 17:06:07 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -2,11 +2,13 @@ module "network" {
   source  = "app.terraform.io/Swat-training/network/google"
   version = "2.5.0"
   # insert required variables here
+  network_name = "swat-network"
+  project_id   = var.project
   subnets = [
-  {
-    subnet_name   = "swat-subnet"
-    subnet_ip     = "10.100.10.0/24"
-    subnet_region = var.region
-  }
-]
+    {
+      subnet_name   = "swat-subnet"
+      subnet_ip     = "10.100.10.0/24"
+      subnet_region = var.region
+    }
+  ]
 }
